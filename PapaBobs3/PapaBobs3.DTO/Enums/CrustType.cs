4ad9e05b38829c6f57e6c5d6f1113db7a45faba7
Gymrat1958 <jeandstpaul@yahoo.com>
2018-07-04 12:19:14 -0400

    Papa Bobs Pizza

---
+++ PapaBobs3/PapaBobs3.DTO/Enums/CrustType.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PapaBobs3.DTO.Enums
+{
+    public enum CrustType
+    {
+        Regular,
+        Thin,
+        Thick
+    }
+}
---
+++ PapaBobs3/PapaBobs3.Domain/PizzaPriceManager.cs
@@ -0,0 +1,81 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PapaBobs3.Domain
+{
+    public class PizzaPriceManager
+    {
+        public static decimal CalculateCost(DTO.OrderDTO order)
+        {
+            decimal cost = 0.0M;
+            var prices = getPizzaPrices();
+            cost += calculateSizeCost(order, prices);
+            cost += calculateCrustCost(order, prices);
+            cost += calculateToppings(order, prices);
+            return cost;
+        }
+
+        private static decimal calculateToppings(DTO.OrderDTO order, DTO.PizzaPriceDTO prices)
+        {
+            decimal cost = 0.0M;
+            cost += (order.Sausage) ? prices.SausageCost : 0M;
+            cost += (order.Pepperoni) ? prices.PepperoniCost : 0M;
+            cost += (order.Onions) ? prices.OnionsCost : 0M;
+            cost += (order.GreenPeppers) ? prices.GreenPeppersCost : 0M;
+            return cost;
+        }
+
+        private static decimal calculateCrustCost(DTO.OrderDTO order, DTO.PizzaPriceDTO prices)
+        {
+            decimal cost = 0.0M;
+            switch (order.Crust)
+            {
+                case PapaBobs3.DTO.Enums.CrustType.Regular:
+                    cost = prices.RegularCrustCost;
+                    break;
+
+                case PapaBobs3.DTO.Enums.CrustType.Thin:
+                    cost = prices.ThinCrustCost;
+                    break;
+
+                case PapaBobs3.DTO.Enums.CrustType.Thick:
+                    cost = prices.ThickCrustCost;
+                    break;
+                default:
+                    break;
+            }
+            return cost;
+        }
+
+        private static decimal calculateSizeCost(DTO.OrderDTO order, DTO.PizzaPriceDTO prices)
+        {
+            decimal cost = 0.0M;
+            switch (order.Size)
+            {
+                case PapaBobs3.DTO.Enums.SizeType.Small:
+                    cost = prices.SmallSizeCost;
+                    break;
+                case PapaBobs3.DTO.Enums.SizeType.Medium:
+                    cost = prices.MediumSizeCost;
+                    break;
+                case PapaBobs3.DTO.Enums.SizeType.Large:
+                    cost = prices.LargeSizeCost;
+                    break;
+                default:
+                    break;
+            }
+            return cost;
+        }
+
+        private static DTO.PizzaPriceDTO getPizzaPrices()
+        {
+            var prices = Persistence.PizzaPriceRepository.GetPizzaPrices();
+            return prices;
+        }
+        
+
+    }
+}
